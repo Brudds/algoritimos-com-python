0a463b57d49541daa5da3e1735edf31d1bfef4a0
Brudds <78767145+Brudds@users.noreply.github.com>
2021-04-09 09:37:19 -0300

    Delete Atividade_2.py

--- Atividade_2.py
+++
@@ -1,7 +0,0 @@
-# Segunda atividade
-
-dia = input("Olá usuário, quero saber sua data de nascimento então por favor digite o dia: ")
-mes = input("Agora, digite o mês (por extenso de preferência): ")
-ano = input("Por fim, digite o ano: ")
-
-print(f"Certo, então você nasceu no dia {dia} de {mes} de {ano}. Correto?")
